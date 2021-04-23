9da58f91b4bb8f0b34c5719fdb1befd33be23f6b
Lucy Donnelly <lucy.donnelly@autotrader.co.uk>
2021-04-23 13:11:49 +0100

    refactor: makeTheString function refactored

--- exercises/javascript/main/refactoring/fizzbuzzgame.js
+++ exercises/javascript/main/refactoring/fizzbuzzgame.js
@@ -3,21 +3,19 @@ let currentNumBetween0And100 = 0;
 let numberType = 0;
 let num5 = 5;
 
+
+
 function makeTheString() {
   let string = "";
 
   for (; currentNumBetween0And100 < 100; currentNumBetween0And100++){
-    string += fizzBuzzer(currentNumBetween0And100) + " "
-    //append return of fizzBuzzer function to empty string
+    string += fizzBuzzer(currentNumBetween0And100) + ' '
+    //append return of fizzBuzzer function to empty string and add a space
   } 
+  console.log(string)
+  // return string.substring(0, string.length - 1); //only removes last bit of whitespace
+  return(string.trim())
   
-  return string.substring(0, string.length - 1);
-  
-}
-
-function buzz() {
-  num5 = 5;
-  return "Buzz";
 }
 
 function fizz() {
@@ -26,18 +24,28 @@ function fizz() {
   
 }
 
+function buzz() {
+  num5 = 5;
+  return 'Buzz';
+}
+
+
+
 function fizzBuzzer(currentNumBetween0And100) {
 
-  numberType++; //1 
-  num5--; //4
+  numberType++; //0 becomes 1 
+  num5--; //5 becomes 4 
 
-  const isAMultipleOfThree = numberType == 0b11; // value of isAMultiplOfThree is aboolean
-  const isAMultipleOfFive = num5 == 0; // value of isAMultiplOfFive is aboolean
+  const isAMultipleOfThree = numberType == 3; // value of isAMultiplOfThree is aboolean - is number type == 3 - true or false?
+  
+
+  const isAMultipleOfFive = num5 == 0; // value of isAMultiplOfFive is aboolean - is num5 == 0 - true or false?
   let s = isAMultipleOfThree || isAMultipleOfFive ? "" : currentNumBetween0And100 + 1;
   
-  if (isAMultipleOfThree) s += fizz();
+  if (isAMultipleOfThree) s += fizz(); 
   if (isAMultipleOfFive) s += buzz();
-  return s;
+  return s; // will return fizz, buzz, fizzbuzz or a number like 1 
+
 }
 
 
